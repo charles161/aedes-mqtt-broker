569dbe408d06ec13cf077b264e6270ce3645ab63
charles161 <fortloop.charles@gmail.com>
2020-04-01 20:47:58 +0530

    Added tcp support

--- aedes.js
+++ aedes.js
@@ -3,18 +3,22 @@ const CERTPATH = "/etc/letsencrypt/live/charlescool.xyz/fullchain.pem";
 
 const fs = require('fs')
 const aedes = require('aedes')()
-const ws = require('websocket-stream')
-const port = 8888
+const sslPort = 8883
+const tcpPort = 8882
 
 const options = {
  key: fs.readFileSync(KEYPATH),
  cert: fs.readFileSync(CERTPATH)
 }
 
-const server = require('tls').createServer(options)
+const server = require('tls').createServer(options, aedes.handle)
 
-ws.createServer({ server: server }, aedes.handle)
+server.listen(sslPort, function () {
+ console.log('server started and listening on sslPort ', sslPort)
+})
 
-server.listen(port, function () {
- console.log('server started and listening on port ', port)
+const tcpserver = require('net').createServer(aedes.handle)
+
+tcpserver.listen(tcpPort, function () {
+ console.log('server started and listening on tcpPort ', tcpPort)
 })
