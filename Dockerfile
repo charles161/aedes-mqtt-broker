422682fda8fc87bbbe22a073312bc7bb8bb0a7a8
charles161 <fortloop.charles@gmail.com>
2020-04-02 13:48:11 +0530

    removed port and host

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:current-slim
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY package.json /usr/src/app/
+RUN npm install
+COPY . /usr/src/app
+EXPOSE 7070 8120
+CMD [ "node", "aedes.js" ]
--- aedes.js
+++ aedes.js
@@ -4,10 +4,7 @@ const CERTPATH = "/etc/letsencrypt/live/charlescool.xyz/fullchain.pem";
 const fs = require('fs')
 const redis = require('mqemitter-redis')
 const mq = redis({
- port: 6379,
- host: '127.0.0.1',
  password: 'tentacles',
- db: 4
 })
 const aedes = require('aedes')(
  {
